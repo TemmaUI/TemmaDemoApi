1326bc89c2b6ba5a2f39d323d697bfa1d85931fc
Yusuff Sodiq <ysodiqakanni@gmail.com>
2024-10-22 22:13:10 -0500

    add money controller

---
+++ TemmaDemoApi/Controllers/MoneyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemmaDemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoneyController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("You just received 1M naira");
+        }
+    }
+}
